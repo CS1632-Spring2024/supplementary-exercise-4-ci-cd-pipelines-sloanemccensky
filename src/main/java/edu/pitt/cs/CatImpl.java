815379e27c33022ab8448d2abdecf60bb9fa203c
sloanemccensky <92398118+sloanemccensky@users.noreply.github.com>
2024-04-12 01:20:20 -0400

    Update CatImpl.java

--- src/main/java/edu/pitt/cs/CatImpl.java
+++ src/main/java/edu/pitt/cs/CatImpl.java
@@ -3,41 +3,94 @@ package edu.pitt.cs;
 public class CatImpl implements Cat {
 
 	// TODO: Fill in with member variables
+	String name;
+	int id;
+	boolean rented;
+	//CatImpl cat = this;
 
 	public CatImpl(int id, String name) {
 		// TODO: Fill in
+		this.id = id;
+		this.name = name;
 	}
 
+	/**
+	 * Rent cat. Simply sets the _rented flag to true.
+	 */
 	public void rentCat() {
 		// TODO: Fill in
+		this.rented = true;
 	}
 
+	/**
+	 * Return cat. Simply sets the _rented flag to false.
+	 */
 	public void returnCat() {
 		// TODO: Fill in
+		this.rented = false;
 	}
 
+	/**
+	 * Rename cat. Simply sets the name to the new name.
+	 * 
+	 * @param String name New name of the cat
+	 */
 	public void renameCat(String name) {
 		// TODO: Fill in
+		this.name = name;
+
 	}
+
+	/**
+	 * Accessor for _name variable. Returns the name of this cat.
+	 * 
+	 * @return String name of cat
+	 */
 
 	public String getName() {
 		// TODO: Fill in
-		return "";
+		return this.name;
+		//return "";
 	}
+
+	/**
+	 * Accessor for _id variable. Returns the ID of this cat.
+	 * 
+	 * @return int ID of this cat
+	 */
 
 	public int getId() {
 		// TODO: Fill in
-		return 0;
+		return this.id;
+		//return 0;
 	}
+
+	/**
+	 * Accessor for _rented variable. Returns if cat is rented.
+	 * 
+	 * @return boolean - true if rented, false otherwise
+	 */
 
 	public boolean getRented() {
 		// TODO: Fill in
-		return false;
+		if( this.rented == true ){
+			return true;
+		}else{
+			return false;
+		}
 	}
+
+	/**
+	 * Returns string version of this cat, in form: "ID *id_num*. *name*" Example
+	 * for cat of ID 1, name Jennyanydots: "ID 1. Jennyanydots"
+	 * 
+	 * @return String string version of cat
+	 */
 
 	public String toString() {
 		// TODO: Fill in
-		return "";
+		String ret = "ID " + this.id + ". " + this.name;
+		return ret;
 	}
 
 }
